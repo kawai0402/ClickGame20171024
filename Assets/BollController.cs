13876bea6bb2cb95a356681d2d1d0ce4846026ee
kouki kawai <nkc.kouki.kawai@gmail.com>
2017-10-26 10:57:25 +0900

    地面作った

---
+++ Assets/BollController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BollController : MonoBehaviour {
+
+    public void Shoot(Vector3 dir)
+    {
+        GetComponent<Rigidbody>().AddForce(dir);
+    }
+	// Use this for initialization
+	void Start () {
+        Shoot(new Vector3(0, 200, 2000));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/BollGenerator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BollGenerator : MonoBehaviour {
+
+    public GameObject BollPrefab;
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject Boll =
+                Instantiate(BollPrefab) as GameObject;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 worldDir = ray.direction;
+            Boll.GetComponent<BollController>().Shoot(
+                new Vector3(0, 200, 2000));
+        }
+
+	}
+}
